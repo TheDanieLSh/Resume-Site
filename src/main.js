9f3b756624af973c8a5583e833f9f125b79e65e1
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2024-01-11 14:37:23 +0300

    f5

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-"use strict";
 console.log('viewport width ->', window.innerWidth);
 console.log('viewport height ->', window.innerHeight);
+
 new fullpage('#fullpage', {
     scrollHorizontally: true,
     scrollingSpeed: 900,
@@ -10,8 +10,9 @@ new fullpage('#fullpage', {
     loopBottom: true,
     loopTop: true,
 });
+
 const works = document.querySelector('.works');
-works.addEventListener('mouseover', (e) => {
+works.addEventListener('mouseover', e => {
     const title = document.querySelector('.work-name');
     if (e.target.classList.contains('work-list__item')) {
         title.innerHTML = e.target.dataset.name;
@@ -20,6 +21,15 @@ works.addEventListener('mouseover', (e) => {
         title.innerHTML = '';
     }
 });
+works.addEventListener('click', e => {
+    if (e.target.classList.contains('work-list__item')) {
+        e.target.classList.add('works__popup');
+        e.target.classList.remove('work-list__item');
+        document.querySelector('.layer').style.display = 'block';
+        e.target.innerHTML = '<p>' + e.target.dataset.description + '</p>' + '<p>' + e.target.dataset.link + '</p>' + '<p>' + e.target.dataset.git + '</p>';
+    }
+})
+
 function moveToInfo() {
     fullpage_api.moveTo(2);
 }
